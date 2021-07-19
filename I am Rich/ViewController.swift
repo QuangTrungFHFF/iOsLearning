b6d6ecfbbc50add15eeec36b9cbfb271e164eb8d
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2021-07-19 11:38:17 +0200

    Add image assets

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by user199027 on 7/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
